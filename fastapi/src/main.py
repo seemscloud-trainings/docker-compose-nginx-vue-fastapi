b29cdaf8c346f8c14fe3c736dccfd51325f001ed
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-12-18 00:48:02 +0100

    test

--- fastapi/src/main.py
+++ fastapi/src/main.py
@@ -5,7 +5,7 @@ from app.v1 import router as v1_routers
 route_v1 = APIRouter()
 route_v1.include_router(router=v1_routers, prefix="/v1")
 
-app = FastAPI(docs_url="/api/v1/docs", openapi_url="/api/v1/openapi.json")
+app = FastAPI(docs_url="/api/docs", openapi_url="/api/openapi.json")
 app.include_router(router=route_v1, prefix="/api")
 
 
