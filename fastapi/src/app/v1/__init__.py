419e3f2cc2243d74d321bfe5f6bcceae6f0301d2
Mateusz Adam Katana <mateusz.adam.katana@gmail.com>
2023-12-18 00:49:00 +0100

    test

--- fastapi/src/app/v1/__init__.py
+++
@@ -1,9 +0,0 @@
-from fastapi import APIRouter
-
-from app.v1.test import router as test_router
-from app.v1.jobs import router as jobs_router
-
-router = APIRouter()
-
-router.include_router(router=test_router, prefix="/test")
-router.include_router(router=jobs_router, prefix="/jobs")
